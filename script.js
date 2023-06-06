7e452afd9fc985f8d431de49485a4b97e0c8cb4b
kadunapikin <ibakoshi27@gmail.com>
2023-06-06 09:22:38 +0100

    update portfolio

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const menu = document.getElementById("myTopnav");
+const navItems = document.getElementById("navItems").getElementsByTagName("a");
+const navItem = document.getElementById("navItem").getElementsByTagName("h3");
 
 function mobileMenu() {
     if (menu.className === "nav-items") {
@@ -9,3 +11,22 @@ function mobileMenu() {
   }
 
 document.getElementById("menuIcon").addEventListener('click', mobileMenu);
+
+function showNavItems() {
+  for (let i = 0; i < navItems.length; i++) {
+    navItems[i].style.opacity = "1";
+    navItems[i].style.transform = "translateY(0)";
+  }
+}
+
+window.addEventListener("load", showNavItems);
+
+function showNavItem() {
+  for (let i = 0; i < navItems.length; i++) {
+    navItem[i].style.opacity = "1";
+    navItem[i].style.transform = "translateY(0)";
+  }
+}
+
+window.addEventListener("load", showNavItem);
+
