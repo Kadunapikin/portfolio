897a242c99e689cb7054c0d20c2879d79b2e9eb8
kadunapikin <ibakoshi27@gmail.com>
2023-06-06 17:28:34 +0100

    fix linters

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-const menu = document.getElementById("myTopnav");
-const navItems = document.getElementById("navItems").getElementsByTagName("a");
-const navItem = document.getElementById("navItem").getElementsByTagName("h3");
+const menu = document.getElementById('myTopnav');
+const navItems = document.getElementById('navItems').getElementsByTagName('a');
+const navItem = document.getElementById('navItem').getElementsByTagName('h3');
 
 function mobileMenu() {
-    if (menu.className === "nav-items") {
-      menu.className += " responsive";
-    } else {
-      menu.className = "nav-items";
-    }
+  if (menu.className === 'nav-items') {
+    menu.className += ' responsive';
+  } else {
+    menu.className = 'nav-items';
   }
+}
 
-document.getElementById("menuIcon").addEventListener('click', mobileMenu);
+document.getElementById('menuIcon').addEventListener('click', mobileMenu);
 
 function showNavItems() {
-  for (let i = 0; i < navItems.length; i++) {
-    navItems[i].style.opacity = "1";
-    navItems[i].style.transform = "translateY(0)";
+  for (let i = 0; i < navItems.length; i += 1) {
+    navItems[i].style.opacity = '1';
+    navItems[i].style.transform = 'translateY(0)';
   }
 }
 
-window.addEventListener("load", showNavItems);
+window.addEventListener('load', showNavItems);
 
 function showNavItem() {
-  for (let i = 0; i < navItems.length; i++) {
-    navItem[i].style.opacity = "1";
-    navItem[i].style.transform = "translateY(0)";
+  for (let i = 0; i < navItems.length; i += 1) {
+    navItem[i].style.opacity = '1';
+    navItem[i].style.transform = 'translateY(0)';
   }
 }
 
-window.addEventListener("load", showNavItem);
-
+window.addEventListener('load', showNavItem);
