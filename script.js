328809705899b8ea228f2f3fa460150f5ff09b7f
kadunapikin <ibakoshi27@gmail.com>
2023-10-07 07:52:29 +0100

    Update mobile responsiveness

--- script.js
+++ script.js
@@ -1,31 +1,8 @@
-const menu = document.getElementById('myTopnav');
-const navItems = document.getElementById('navItems').getElementsByTagName('a');
-// const navBarName = document.getElementById('navItem').getElementsByTagName('h3');
-
-function mobileMenu() {
-  if (menu.className === 'nav-items') {
-    menu.className += ' responsive';
+document.getElementById("menuIcon").addEventListener("click", function() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "nav-items") {
+      x.className += " responsive";
   } else {
-    menu.className = 'nav-items';
+      x.className = "nav-items";
   }
-}
-
-document.getElementById('menuIcon').addEventListener('click', mobileMenu);
-
-function showNavItems() {
-  for (let i = 0; i < navItems.length; i += 1) {
-    navItems[i].style.opacity = '1';
-    navItems[i].style.transform = 'translateY(0)';
-  }
-}
-
-window.addEventListener('load', showNavItems);
-
-// function showNavBarName() {
-//   for (let i = 0; i < navItems.length; i += 1) {
-//     navBarName[i].style.opacity = '1';
-//     navBarName[i].style.transform = 'translateY(0)';
-//   }
-// }
-
-// window.addEventListener('load', showNavBarName);
+});
