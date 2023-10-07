b38172399f064adebe1c2989bd3e926a7f09f512
kadunapikin <ibakoshi27@gmail.com>
2023-10-07 08:18:46 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById("menuIcon").addEventListener("click", function() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "nav-items") {
-      x.className += " responsive";
+document.getElementById('menuIcon').addEventListener('click', () => {
+  const x = document.getElementById('myTopnav');
+  if (x.className === 'nav-items') {
+    x.className += ' responsive';
   } else {
-      x.className = "nav-items";
+    x.className = 'nav-items';
   }
 });
